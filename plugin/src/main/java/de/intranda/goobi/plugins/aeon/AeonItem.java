3cc53fdbde7749df07a8cf3764acd9744ef9d958
Maxim Strzebkowski <maxim.strzebkowski@intranda.com>
2021-08-23 13:15:16 +0200

    Added Collapse Buttons and changed Transmission Details to input fields

--- plugin/src/main/java/de/intranda/goobi/plugins/aeon/AeonItem.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/aeon/AeonItem.java
@@ -8,7 +8,10 @@ public class AeonItem {
 	private String title;
 	private String publicationType;
 	private String shelfmark;
+	
 	private boolean accepted = false;
+	
+	private boolean showDetails = false;
 	
 	public void decline() {
 		System.out.println("declined");
