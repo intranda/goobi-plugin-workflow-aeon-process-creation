a00210f59a22d9ea8bbf4ef707c2d3552e109b3f
Robert Sehr <robert.sehr@intranda.com>
2022-03-21 10:39:49 +0100

    popup layout

--- plugin/src/main/java/de/intranda/goobi/plugins/aeon/AeonRecord.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/aeon/AeonRecord.java
@@ -36,13 +36,13 @@ public class AeonRecord {
     public String getDuplicationPopup() {
         StringBuilder answer = new StringBuilder();
 
-        answer.append("<ul style=\"list-style-type:none;\">");
+        answer.append("<ul style=\"list-style-type:none;padding-left: 0; \">");
         for (AeonProperty property : duplicateProperties) {
             if (StringUtils.isNotBlank(property.getValue())) {
                 answer.append("<li class=\"key-value-pair\">");
                 answer.append("<b>");
                 answer.append(property.getTitle());
-                answer.append("</b>: ");
+                answer.append("</b>:</li><li> ");
                 answer.append(property.getValue());
                 answer.append("</li>");
             }
