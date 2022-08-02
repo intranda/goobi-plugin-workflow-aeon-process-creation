be3eed94e63aed35343b843380002ad4ef9f727d
Robert Sehr <robert.sehr@intranda.com>
2022-08-02 14:18:15 +0200

    changed test class

--- plugin/src/test/java/de/intranda/goobi/plugins/AeonProcessCreationPluginTest.java
+++ plugin/src/test/java/de/intranda/goobi/plugins/AeonProcessCreationPluginTest.java
@@ -3,10 +3,28 @@ package de.intranda.goobi.plugins;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AeonProcessCreationPluginTest {
+
+    private static String resourcesFolder;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        resourcesFolder = "src/test/resources/"; // for junit tests in eclipse
+
+        if (!Files.exists(Paths.get(resourcesFolder))) {
+            resourcesFolder = "target/test-classes/"; // to run mvn test from cli or in jenkins
+        }
+
+        String log4jFile = resourcesFolder + "log4j2.xml"; // for junit tests in eclipse
+
+        System.setProperty("log4j.configurationFile", log4jFile);
+    }
 
     @Test
     public void testVersion() throws IOException {
